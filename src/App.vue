9350bb3137feeeef748a172731b20606809477bf
Alexander Lyutov <lyutov.alexander@e-queo.com>
2022-11-27 12:26:53 +0300

    blank

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <AppShop />
+</template>
+
+<script lang="ts">
+declare module '@/components/layouts/shop.vue';
+import AppShop from '@/components/layouts/shop.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    AppShop,
+  }
+}
+</script>
---
+++ src/components/layouts/shop.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="shopping-cart">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shop',
+};
+</script>
